9b7bab987a73f727a6970c6e3c8a8739da3eda0e
Jon <Jon>
2017-03-05 20:00:06 +0800

    add picture component

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,23 +4,46 @@ require('styles/App.scss');
 import React from 'react';
 
 //获取图片相关的数据
-var imageDatas = require('../data/imageDatas.json');
+let imageDatas = require('json!../data/imageDatas.json');
 //利用自执行函数将图片名信息转成图片URL路径信息
 imageDatas = (function genImageURL(imageDataArr) {
 	for (var i = 0, j = imageDataArr.length; i < j; i++) {
-		var singleImageData = imageDataArr[i];
+		let singleImageData = imageDataArr[i];
 		singleImageData.imageURL = require('../images/' + singleImageData.fileName);
 		imageDataArr[i] = singleImageData;
 	}
 	return imageDataArr;
 })(imageDatas);
 
-class AppComponent extends React.Component {
+class ImgFigure extends React.Component {
 	render() {
 		return (
-			<section className="state">
-				<section className="img-sec"></section>
-				<nav className="controller-nav"></nav>
+			<figure className="img-figure">
+				<img src={this.props.data.imageURL} alt={this.props.data.title}/>
+				<figcaption>
+					<h2>{this.props.data.title}</h2>
+				</figcaption>
+			</figure>
+		);
+	}
+}
+
+class AppComponent extends React.Component {
+	render() {
+
+		var controllerUnits = [],
+			imgFigures = [];
+
+		imageDatas.forEach(value => imgFigures.push(<ImgFigure data={value}/>));
+
+		return (
+			<section className="stage">
+				<section className="img-sec">
+					{imgFigures}
+				</section>
+				<nav className="controller-nav">
+					{controllerUnits}
+				</nav>
 			</section>
 		);
 	}
